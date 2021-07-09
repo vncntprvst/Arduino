64d545e3f5f3202eee095562b1816edd22091db0
Vincent Prevosto <vincent.prevosto@duke.edu>
2021-07-09 14:57:51 -0400

    cleaned up version

--- Sync/DigTrigFast_elapsedMicros_HSCam/DigTrigFast_elapsedMicros_HSCam.ino
+++ Sync/DigTrigFast_elapsedMicros_HSCam/DigTrigFast_elapsedMicros_HSCam.ino
@@ -1,9 +1,9 @@
 // Trigger signal for High-speed camera
+
 const int syncPin = 0;
 const int syncTrigPin = 1;
 elapsedMicros sinceOnsetTest;
 elapsedMicros pulseDuration;
-//int counter = 0;
 bool syncOn = false; //true
 
 void setup() {
@@ -16,14 +16,9 @@ void loop() {
   if (syncOn == true) {
     // trigger at 500Hz
     if (sinceOnsetTest >= 2000){
-//    if ((sinceOnsetTest >= (2000 + (5000000 - (counter *10)))) & (counter<500000)) {
       sinceOnsetTest = 0;
       SendPulse();
-//      counter += 1;
-//      digitalWriteFast(syncPin, !digitalReadFast(syncPin));
-//      digitalWriteFast(syncPin, HIGH);
     } else {
-//      digitalWriteFast(syncPin, LOW);
       TestSync();
     }
   } else {
